7388156f51d3aedd4bbc5b1482cb9c29e5b278fa
cadumd <cadu.md2021@gmail.com>
2025-12-06 11:23:45 -0300

    alteracao1

--- API/index.js
+++ API/index.js
@@ -11,17 +11,12 @@ const servidor = http.createServer((pedido, resposta) => {
 
         case '/style.css':
             resposta.writeHead(200, { 'Conten-Type': 'text/css' })
-            resposta.end(fs.readFileSync("./style.css"))
+            resposta.end(fs.readFileSync("./frontend/style.css"))
             break
 
         case '/script.js':
             resposta.writeHead(200, { 'Conten-Type': 'text/javascript' })
             resposta.end(fs.readFileSync("./script.js"))
-            break
-
-        case '/teste1.html':
-            resposta.writeHead(200, { 'Conten-Type': 'text/html' })
-            resposta.end(fs.readFileSync("./teste1.html"))
             break
     }
 })
